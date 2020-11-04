e756071fd35c6892382c6f88f77361ed003c01e7
Andrew <andrew.casin@gmail.com>
2020-11-03 22:46:46 -0600

    commit tree 5cf8929f122930dd1c41cddb87b1469dd7b38b7c

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+# require 'pry'
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        array.select {|w| w.split("").sort == @word.split("").sort}
+    end
+
+    # def match(possible)
+    #     array = []
+    #     possible.each do |pword|
+    #         if pword.sort = @word.sort
+    #             pword << array
+    #         end
+    #     end
+    #     array
+    # end
+end
+
+# binding.pry
